34bdc3469772a5f452c3a365bdd4443b048709f6
balqishah <balqishah.siti@gmail.com>
2017-12-18 10:39:00 +0800

    tukar url

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from django.conf.urls import url,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     url(r'', include('blog.urls')),
 ]
 
